2cbe3c510aeb6c24d2a6ef069e3a9698290198dc
Priyanka91400 <136422890+Priyanka91400@users.noreply.github.com>
2023-07-22 20:31:25 +0530

    schedule updated

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,17 @@
+import Button from './component.js';
+import {MyButton} from './component';
 function Heading(){
   return(
-    <h1>Heading</h1>
+    <h1>TRAINS SCHEDULE</h1>
   );
 }
-
+                                                                                                    
 export default function app(){
   return(
+    <>
     <Heading/>
+    <Button/>
+    <MyButton/>
+    </>
   );
 }
---
+++ my-app/src/component.js
@@ -0,0 +1,14 @@
+function Button(){
+    return(
+        <button>Get all trains schedule</button>
+    );
+}
+
+
+export function MyButton(){
+    return(
+        <button>Get single train schedule</button>
+    );
+}
+
+export default Button;
